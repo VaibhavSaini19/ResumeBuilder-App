7082f71b385ac9a0bbe04d008ea8555388fde9e0
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-04-25 18:58:02 +0530

    Add download functionality for resume

--- app/src/main/java/com/example/resumebuilder/ViewCVActivity.java
+++ app/src/main/java/com/example/resumebuilder/ViewCVActivity.java
@@ -4,15 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +30,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.gson.Gson;
 
 import java.net.URLEncoder;
@@ -31,6 +43,7 @@ public class ViewCVActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
+    private StorageReference storageReference;
 
     Gson gson = new Gson();
     String json, postData;
@@ -54,13 +67,14 @@ public class ViewCVActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("users/"+user.getUid()+"/profiles");
+        storageReference = FirebaseStorage.getInstance().getReference("users/"+user.getUid());
 
         databaseReference.child(ProfileId)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         userProfile = dataSnapshot.getValue(Profile.class);
-
+//                        Task d = storageReference.child(userProfile.getProfilePic()).getBytes(1024*1024);
                         json = gson.toJson(userProfile);
                         try {
                             postData = "data=" + URLEncoder.encode(json, "UTF-8");
@@ -88,19 +102,14 @@ public class ViewCVActivity extends AppCompatActivity {
         webView.getSettings().setUseWideViewPort(true);
         webView.setInitialScale(100);
 //        webView.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3");
-
         webView.setVerticalScrollBarEnabled(true);
         webView.setHorizontalScrollBarEnabled(true);
         webView.setScrollbarFadingEnabled(false);
-
         webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
-
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-
-
         webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
                 progressBar.setProgress(progress);
@@ -112,5 +121,43 @@ public class ViewCVActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void startDownloadCV(){
+        createWebPrintJob(webView);
+    }
+    private void createWebPrintJob(WebView webView) {
+
+        // Get a PrintManager instance
+        PrintManager printManager = (PrintManager) this.getSystemService(Context.PRINT_SERVICE);
+
+        String jobName = getString(R.string.app_name) + " Document";
+
+        // Get a print adapter instance
+        PrintDocumentAdapter printAdapter = webView.createPrintDocumentAdapter(jobName);
+
+        // Create a print job with name and adapter instance
+        PrintJob printJob = printManager.print(jobName, printAdapter, new PrintAttributes.Builder().build());
+
+        // Save the job object for later status checking
+//        printJobs.add(printJob);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.download_btn:
+                startDownloadCV();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
